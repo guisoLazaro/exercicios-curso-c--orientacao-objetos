bfba234d977931f9cacf698f0405d4127958161f
gsolazaro <gsolazaro@gmail.com>
2023-01-05 21:01:41 -0300

    Finalização sessão 4 com exercicios de fixação sobre membros estáticos, criação de um conversor de moeda

---
+++ ExerciciosFixacaoSessao4/ExMembrosEstaticos/ExMembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExMembrosEstaticos {
+    internal class ConversorDeMoeda {
+
+        public static double Converter(double conversor, double quantidade) {
+            double valor = conversor * quantidade;
+            double valorImposto = valor + (valor * 6.0) / 100;
+            return valorImposto;
+        }
+
+    }
+}
---
+++ ExerciciosFixacaoSessao4/ExMembrosEstaticos/ExMembrosEstaticos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExMembrosEstaticos {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação do dolar: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares você vai comprar? ");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em Reais: " + ConversorDeMoeda.Converter(cotacao, qtd).ToString("F2", CultureInfo.CurrentCulture));
+        }
+    }
+}
